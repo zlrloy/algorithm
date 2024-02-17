4af60cc3c929e89f5bcd1ee8da75ab92ea5b75d3
jieunyang <asd654@hanmail.net>
2024-02-17 17:03:19 +0900

    add: 글자 이어 붙여 문자열 만들기 JavaScript 제출

---
+++ Programers/Lv. 0/글자 이어 붙여 문자열 만들기.js
@@ -0,0 +1,18 @@
+/*
+문제. 글자 이어 붙여 문자열 만들기
+문자열 my_string과 정수 배열 index_list가 매개변수로 주어집니다. my_string의 index_list의 원소들에 해당하는 인덱스의 글자들을 순서대로 이어 붙인 문자열을 return 하는 solution 함수를 작성해 주세요.
+*/
+
+//Case1
+function solution(my_string, index_list) {
+  let result = "";
+  for (let i = 0; i < index_list.length; i++) {
+    result += my_string[index_list[i]];
+  }
+  return result;
+}
+
+console.log(
+  solution("cvsgiorszzzmrpaqpe", [16, 6, 5, 3, 12, 14, 11, 11, 17, 12, 7])
+); //"programmers"
+console.log(solution("zpiaz", [1, 2, 0, 0, 3])); //"pizza"
