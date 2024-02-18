712fa6598e7c63b0f76cc674351533ede57d3d87
jieunyang <asd654@hanmail.net>
2024-02-18 09:42:13 +0900

    update: 글자 이어 붙여 문자열 만들기 Case2 추가

--- Programers/Lv. 0/글자 이어 붙여 문자열 만들기.js
+++ Programers/Lv. 0/글자 이어 붙여 문자열 만들기.js
@@ -16,3 +16,8 @@ console.log(
   solution("cvsgiorszzzmrpaqpe", [16, 6, 5, 3, 12, 14, 11, 11, 17, 12, 7])
 ); //"programmers"
 console.log(solution("zpiaz", [1, 2, 0, 0, 3])); //"pizza"
+
+//Case2
+function solution(my_string, index_list) {
+  return index_list.reduce((result, i) => result + my_string[i], "");
+}
