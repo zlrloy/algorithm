d30d09cb39e2c7e8febeee0cc933f04174c8e0c3
jieunyang <asd654@hanmail.net>
2024-02-28 16:39:38 +0900

    add: 문자열 잘라서 정렬하기 JavaScript 제출

---
+++ Programers/Lv. 0/문자열 잘라서 정렬하기.js
@@ -0,0 +1,19 @@
+/*
+문제. 문자열 잘라서 정렬하기
+문자열 myString이 주어집니다. "x"를 기준으로 해당 문자열을 잘라내 배열을 만든 후 사전순으로 정렬한 배열을 return 하는 solution 함수를 완성해 주세요.
+단, 빈 문자열은 반환할 배열에 넣지 않습니다.
+
+풀이.
+//문자열 안에 x만 걸러내고
+//오름차순 정렬
+*/
+
+function solution(myString) {
+  return myString
+    .split("x")
+    .sort()
+    .filter((el) => el);
+}
+
+console.log(solution("axbxcxdx")); //["a","b","c","d"]
+console.log(solution("dxccxbbbxaaaa")); //["aaaa","bbb","cc","d"]
