4e944e5e10a687928caa59378c3ccce307220ea8
jieunyang <asd654@hanmail.net>
2024-04-01 23:10:07 +0900

    update: 세로 읽기 Case2 추가

--- Programers/Lv. 0/세로 읽기.js
+++ Programers/Lv. 0/세로 읽기.js
@@ -3,6 +3,7 @@
 문자열 my_string과 두 정수 m, c가 주어집니다. my_string을 한 줄에 m 글자씩 가로로 적었을 때 왼쪽부터 세로로 c번째 열에 적힌 글자들을 문자열로 return 하는 solution 함수를 작성해 주세요.
 */
 
+//Case1
 function solution(my_string, m, c) {
   let result = "";
 
@@ -15,3 +16,8 @@ function solution(my_string, m, c) {
 
 console.log(solution("ihrhbakrfpndopljhygc", 4, 2)); //"happy"
 console.log(solution("programmers", 1, 1)); //"programmers"
+
+//Case2
+function solution(my_string, m, c) {
+  return [...my_string].filter((_, i) => i % m === c - 1).join("");
+}
