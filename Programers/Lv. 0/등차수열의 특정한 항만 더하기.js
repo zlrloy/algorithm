a76552b90bf7bb79fb51aba39ef1865d42daa762
jieunyang <asd654@hanmail.net>
2024-04-07 22:01:16 +0900

    update: 등차수열의 특정한 항만 더하기 Case2 추가

--- Programers/Lv. 0/등차수열의 특정한 항만 더하기.js
+++ Programers/Lv. 0/등차수열의 특정한 항만 더하기.js
@@ -4,6 +4,7 @@
 이 등차수열의 1항부터 n항까지 included가 true인 항들만 더한 값을 return 하는 solution 함수를 작성해 주세요.
 */
 
+//Case1
 function solution(a, d, included) {
   const sequence = [];
 
@@ -17,3 +18,10 @@ function solution(a, d, included) {
 
 console.log(solution(3, 4, [true, false, false, true, true])); //37
 console.log(solution(7, 1, [false, false, false, true, false, false, false])); //10
+
+//Case2
+function solution(a, d, included) {
+  return included.reduce((acc, flag, i) => {
+    return flag ? acc + a + d * i : acc;
+  }, 0);
+}
