84926c9b0460be2e9a62e9516d8c5d6419a04be8
jieunyang <asd654@hanmail.net>
2024-04-09 18:14:31 +0900

    update: 등차수열의 특정한 항만 더하기 Case4 추가

--- Programers/Lv. 0/등차수열의 특정한 항만 더하기.js
+++ Programers/Lv. 0/등차수열의 특정한 항만 더하기.js
@@ -34,3 +34,12 @@ function solution(a, d, included) {
   });
   return answer;
 }
+
+//Case4
+function solution(a, d, included) {
+  return Array(included.length)
+    .fill(a)
+    .map((x, idx) => x + idx * d)
+    .map((x, idx) => (included[idx] ? x : 0))
+    .reduce((a, b) => a + b);
+}
