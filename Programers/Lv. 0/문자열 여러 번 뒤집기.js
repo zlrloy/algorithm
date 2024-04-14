3a231d0fa2ed8539ee2b6aa9f2fe632832e50ca7
yjieuny <asd654@hanmail.net>
2024-04-14 11:15:56 +0900

    add: 문자열 여러 번 뒤집기 JavaScript 제출

---
+++ Programers/Lv. 0/문자열 여러 번 뒤집기.js
@@ -0,0 +1,27 @@
+/*
+문제. 문자열 여러 번 뒤집기
+문자열 my_string과 이차원 정수 배열 queries가 매개변수로 주어집니다.
+queries의 원소는 [s, e] 형태로, my_string의 인덱스 s부터 인덱스 e까지를 뒤집으라는 의미입니다.
+my_string에 queries의 명령을 순서대로 처리한 후의 문자열을 return 하는 solution 함수를 작성해 주세요.
+*/
+
+function solution(my_string, queries) {
+  return queries
+    .reduce(
+      (acc, [s, e]) => {
+        acc.splice(s, e - s + 1, ...acc.slice(s, e + 1).reverse());
+        return acc;
+      },
+      [...my_string]
+    )
+    .join("");
+}
+
+console.log(
+  solution("rermgorpsam", [
+    [2, 3],
+    [0, 7],
+    [5, 9],
+    [6, 10],
+  ])
+); //"programmers"
