499371ad420111a3582057f1bafdcd1f72c67333
yjieuny <asd654@hanmail.net>
2024-04-15 22:26:44 +0900

    update: 문자열 여러 번 뒤집기 Case2 추가

--- Programers/Lv. 0/문자열 여러 번 뒤집기.js
+++ Programers/Lv. 0/문자열 여러 번 뒤집기.js
@@ -5,6 +5,7 @@ queries의 원소는 [s, e] 형태로, my_string의 인덱스 s부터 인덱스 
 my_string에 queries의 명령을 순서대로 처리한 후의 문자열을 return 하는 solution 함수를 작성해 주세요.
 */
 
+//Case1
 function solution(my_string, queries) {
   return queries
     .reduce(
@@ -25,3 +26,13 @@ console.log(
     [6, 10],
   ])
 ); //"programmers"
+
+//Case2
+function solution(my_string, queries) {
+  let str = my_string.split("");
+  queries.forEach(([start, end]) => {
+    const changeStr = str.slice(start, end + 1);
+    str.splice(start, changeStr.length, ...changeStr.reverse());
+  });
+  return str.join("");
+}
