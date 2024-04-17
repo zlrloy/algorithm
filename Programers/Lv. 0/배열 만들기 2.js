55c318c94d8d309f2625b2d868d407db587504a5
yjieuny <asd654@hanmail.net>
2024-04-17 13:32:17 +0900

    add: 배열 만들기2 JavaScript 제출

---
+++ Programers/Lv. 0/배열 만들기 2.js
@@ -0,0 +1,20 @@
+/*
+문제. 배열 만들기 2
+정수 l과 r이 주어졌을 때, l 이상 r이하의 정수 중에서 숫자 "0"과 "5"로만 이루어진 모든 정수를 오름차순으로 저장한 배열을 return 하는 solution 함수를 완성해 주세요.
+만약 그러한 정수가 없다면, -1이 담긴 배열을 return 합니다.
+*/
+
+function solution(l, r) {
+  const result = [];
+
+  for (let i = l; i <= r; i++) {
+    if (/^[05]+$/.test(String(i))) {
+      result.push(i);
+    }
+  }
+
+  return result.length === 0 ? [-1] : result;
+}
+
+console.log(solution(5, 555)); //[5, 50, 55, 500, 505, 550, 555]
+console.log(solution(10, 20)); //[-1]
