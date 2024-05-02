3c22f84a7e7a5576a93d021de91125e0aa2d583b
yjieuny <asd654@hanmail.net>
2024-05-02 22:09:57 +0900

    add: 이차원 배열 대각선 순회하기 JavaScript 제출

---
+++ Programers/Lv. 0/이차원 배열 대각선 순회하기.js
@@ -0,0 +1,29 @@
+/*
+문제. 이차원 배열 대각선 순회하기
+2차원 정수 배열 board와 정수 k가 주어집니다.
+i + j <= k를 만족하는 모든 (i, j)에 대한 board[i][j]의 합을 return 하는 solution 함수를 완성해 주세요.
+*/
+
+function solution(board, k) {
+  var answer = 0;
+  for (let i = 0; i < board.length; i += 1) {
+    for (let j = 0; j < board[0].length; j += 1) {
+      if (i + j <= k) {
+        answer += board[i][j];
+      }
+    }
+  }
+  return answer;
+}
+
+console.log(
+  solution(
+    [
+      [0, 1, 2],
+      [1, 2, 3],
+      [2, 3, 4],
+      [3, 4, 5],
+    ],
+    2
+  )
+); //8
