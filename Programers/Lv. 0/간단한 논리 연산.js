9673391a317d6f9e7bf38b11808246d1784849b1
yjieuny <asd654@hanmail.net>
2024-05-05 23:38:51 +0900

    add: 간단한 논리 연산 JavaScript 제출

---
+++ Programers/Lv. 0/간단한 논리 연산.js
@@ -0,0 +1,12 @@
+/*
+문제. 간단한 논리 연산
+boolean 변수 x1, x2, x3, x4가 매개변수로 주어질 때, 다음의 식의 true/false를 return 하는 solution 함수를 작성해 주세요.
+(x1 ∨ x2) ∧ (x3 ∨ x4)
+*/
+
+function solution(x1, x2, x3, x4) {
+  return (x1 || x2) && (x3 || x4);
+}
+
+console.log(solution(false, true, true, true)); //true
+console.log(solution(true, false, false, false)); //false
