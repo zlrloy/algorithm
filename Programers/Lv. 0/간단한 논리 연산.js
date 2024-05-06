5cf7d02159fae51b31659f84983673666b7e854d
yjieuny <asd654@hanmail.net>
2024-05-06 22:19:28 +0900

    update: 간단한 논리 연산 Case2 추가

--- Programers/Lv. 0/간단한 논리 연산.js
+++ Programers/Lv. 0/간단한 논리 연산.js
@@ -4,9 +4,15 @@ boolean 변수 x1, x2, x3, x4가 매개변수로 주어질 때, 다음의 식의
 (x1 ∨ x2) ∧ (x3 ∨ x4)
 */
 
+//Case1
 function solution(x1, x2, x3, x4) {
   return (x1 || x2) && (x3 || x4);
 }
 
 console.log(solution(false, true, true, true)); //true
 console.log(solution(true, false, false, false)); //false
+
+//Case2
+function solution(x1, x2, x3, x4) {
+  return (x1 + x2) * (x3 + x4) ? true : false;
+}
