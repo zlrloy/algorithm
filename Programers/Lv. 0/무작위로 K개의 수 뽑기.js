dd3934377feecea928443d24f4294b1772c4633d
yjieuny <asd654@hanmail.net>
2024-05-13 17:36:12 +0900

    update: 무작위로 K개의 수 뽑기 Case3 추가

--- Programers/Lv. 0/무작위로 K개의 수 뽑기.js
+++ Programers/Lv. 0/무작위로 K개의 수 뽑기.js
@@ -23,3 +23,8 @@ function solution(arr, k) {
   const set = new Set(arr);
   return set.size < k ? [...set, ...Array(k - set.size).fill(-1)] : [...set].slice(0, k);
 }
+
+//Case3
+function solution(arr, k) {
+  return [...Array.from(new Set(arr)), ...new Array(k).fill(-1)].slice(0, k);
+}
