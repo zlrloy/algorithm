51db69877d3bf2ddd556366fcd2a874ab249d57c
yjieuny <asd654@hanmail.net>
2024-05-26 22:50:45 +0900

    update: 수열과 구간 쿼리3 Case3 추가

--- Programers/Lv. 0/수열과 구간 쿼리 3.js
+++ Programers/Lv. 0/수열과 구간 쿼리 3.js
@@ -34,3 +34,11 @@ function solution(arr, queries) {
   });
   return arr;
 }
+
+//Case3
+function solution(arr, queries) {
+  for (let [i, j] of queries) {
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
