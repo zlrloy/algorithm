32ad3de5e84352178a8cc57acceac8e1c9cd1b2f
yjieuny <asd654@hanmail.net>
2024-06-07 19:19:57 +0900

    update: 배열 만들기4 Case2 추가

--- Programers/배열 만들기 4.js
+++ Programers/배열 만들기 4.js
@@ -24,3 +24,18 @@ function solution(arr) {
 }
 
 console.log(solution([1, 4, 2, 5, 3])); //[1, 2, 3]
+
+//Case2
+function solution(arr) {
+  let stk = [];
+  for (let i = 0; i < arr.length; ) {
+    if (stk.length === 0) {
+      stk.push(arr[i++]);
+    } else if (stk[stk.length - 1] < arr[i]) {
+      stk.push(arr[i++]);
+    } else if (stk[stk.length - 1] >= arr[i]) {
+      stk.pop();
+    }
+  }
+  return stk;
+}
