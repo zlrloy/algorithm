8822ee71102059890d9739ec081a2ffec45b9edd
yjieuny <asd654@hanmail.net>
2024-06-15 21:34:06 +0900

    update: 수열과 구간 쿼리2 Case4 추가

--- Programers/Lv. 0/수열과 구간 쿼리 2.js
+++ Programers/Lv. 0/수열과 구간 쿼리 2.js
@@ -51,3 +51,11 @@ function solution(arr, queries) {
     return result;
   });
 }
+
+//Case4
+function solution(arr, queries) {
+  return queries.map(([s, e, k]) => {
+    const result = arr.slice(s, e + 1).filter((num) => num > k);
+    return result.length > 0 ? Math.min(...result) : -1;
+  });
+}
