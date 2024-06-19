22c3cb2cee2a58b45bba0ecc77dc1def08bf19ab
yjieuny <asd654@hanmail.net>
2024-06-19 10:58:48 +0900

    add: 그림 확대 JavaScript 제출

---
+++ Programers/Lv. 0/그림 확대.js
@@ -0,0 +1,27 @@
+/*
+문제. 그림 확대
+직사각형 형태의 그림 파일이 있고, 이 그림 파일은 1 × 1 크기의 정사각형 크기의 픽셀로 이루어져 있습니다.
+이 그림 파일을 나타낸 문자열 배열 picture과 정수 k가 매개변수로 주어질 때, 이 그림 파일을 가로 세로로 k배 늘린 그림 파일을 나타내도록 문자열 배열을 return 하는 solution 함수를 작성해 주세요.
+*/
+
+//Case1
+function solution(picture, k) {
+  const tempArr = picture.map((x) =>
+    x
+      .split("")
+      .map((x1) => x1.repeat(k))
+      .join("")
+  );
+
+  const newArr = [];
+  for (let i = 0; i < tempArr.length; i++) {
+    for (let j = 0; j < k; j++) {
+      newArr.push(tempArr[i]);
+    }
+  }
+
+  return newArr;
+}
+
+console.log(solution([".xx...xx.", "x..x.x..x", "x...x...x", ".x.....x.", "..x...x..", "...x.x...", "....x...."], 2)); //["..xxxx......xxxx..", "..xxxx......xxxx..", "xx....xx..xx....xx", "xx....xx..xx....xx", "xx......xx......xx", "xx......xx......xx", "..xx..........xx..", "..xx..........xx..", "....xx......xx....", "....xx......xx....", "......xx..xx......", "......xx..xx......", "........xx........", "........xx........"]
+console.log(solution(["x.x", ".x.", "x.x"], 3)); //["xxx...xxx", "xxx...xxx", "xxx...xxx", "...xxx...", "...xxx...", "...xxx...", "xxx...xxx", "xxx...xxx", "xxx...xxx"]
