74b3bc3166aa005d482f69fe15f1266346946325
yjieuny <asd654@hanmail.net>
2024-06-21 11:01:13 +0900

    update: 그림 확대 Case3 추가

--- Programers/Lv. 0/그림 확대.js
+++ Programers/Lv. 0/그림 확대.js
@@ -37,3 +37,16 @@ function solution(picture, k) {
 
   return result;
 }
+
+//Case3
+function solution(picture, k) {
+  var answer = [];
+
+  picture.forEach((line) => {
+    const expanded = [...line].reduce((acc, cur) => acc + cur.repeat(k), "");
+
+    for (let i = 0; i < k; i++) answer.push(expanded);
+  });
+
+  return answer;
+}
