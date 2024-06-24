83678889bc5f3b079bc36b587fc696994e1b71c1
yjieuny <asd654@hanmail.net>
2024-06-24 10:27:03 +0900

    add: 전국 대회 선발 고사 JavaScript 제출

---
+++ Programers/Lv. 0/전국 대회 선발 고사.js
@@ -0,0 +1,18 @@
+/*
+문제. 전국 대회 선발 고사
+0번부터 n - 1번까지 n명의 학생 중 3명을 선발하는 전국 대회 선발 고사를 보았습니다. 등수가 높은 3명을 선발해야 하지만, 개인 사정으로 전국 대회에 참여하지 못하는 학생들이 있어 참여가 가능한 학생 중 등수가 높은 3명을 선발하기로 했습니다.
+각 학생들의 선발 고사 등수를 담은 정수 배열 rank와 전국 대회 참여 가능 여부가 담긴 boolean 배열 attendance가 매개변수로 주어집니다.
+전국 대회에 선발된 학생 번호들을 등수가 높은 순서대로 각각 a, b, c번이라고 할 때 10000 × a + 100 × b + c를 return 하는 solution 함수를 작성해 주세요.
+*/
+
+function solution(rank, attendance) {
+  const [a, b, c] = rank
+    .map((r, i) => [r, i])
+    .filter(([_, i]) => attendance[i])
+    .sort(([a], [b]) => a - b);
+  return 10000 * a[1] + 100 * b[1] + c[1];
+}
+
+console.log(solution([3, 7, 2, 5, 4, 6, 1], [false, true, true, true, true, false, false])); //20403
+console.log(solution([1, 2, 3], [true, true, true])); //102
+console.log(solution([6, 1, 5, 2, 3, 4], [true, false, true, false, false, true])); //50200
