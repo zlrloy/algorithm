c9cd96fcf7ed25c696267cb2b5b599742a0a7234
yjieuny <asd654@hanmail.net>
2024-06-28 23:30:15 +0900

    add: 코드 처리하기 JavaScript 제출

--- Programers/Lv. 0/전국 대회 선발 고사.js
+++ Programers/Lv. 0/전국 대회 선발 고사.js
@@ -5,6 +5,7 @@
 전국 대회에 선발된 학생 번호들을 등수가 높은 순서대로 각각 a, b, c번이라고 할 때 10000 × a + 100 × b + c를 return 하는 solution 함수를 작성해 주세요.
 */
 
+//Case1
 function solution(rank, attendance) {
   const [a, b, c] = rank
     .map((r, i) => [r, i])
