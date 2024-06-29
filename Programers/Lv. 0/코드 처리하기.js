1b55943ac836ab5f71c67a059d021ecae694cf43
yjieuny <asd654@hanmail.net>
2024-06-29 23:31:14 +0900

    update: 코드 처리하기 Case2 추가

--- Programers/Lv. 0/코드 처리하기.js
+++ Programers/Lv. 0/코드 처리하기.js
@@ -31,3 +31,20 @@ function solution(code) {
 }
 
 console.log(solution("abc1abc1abc")); //"acbac"
+
+//Case2
+function solution(code) {
+  let answer = "";
+  let mode = 0;
+
+  for (let i = 0; i < code.length; i += 1) {
+    if (Number(code[i]) === 1) {
+      mode = mode === 1 ? 0 : 1;
+    }
+    if (Number(code[i]) !== 1 && i % 2 === mode) {
+      answer += code[i];
+    }
+  }
+
+  return answer.length > 0 ? answer : "EMPTY";
+}
