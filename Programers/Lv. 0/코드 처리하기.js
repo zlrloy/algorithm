653e59c52d60f48c9784480ee617cc681205ed12
yjieuny <asd654@hanmail.net>
2024-07-04 16:28:29 +0900

    update: 코드 처리하기 Case3 추가

--- Programers/Lv. 0/코드 처리하기.js
+++ Programers/Lv. 0/코드 처리하기.js
@@ -48,3 +48,17 @@ function solution(code) {
 
   return answer.length > 0 ? answer : "EMPTY";
 }
+
+//Case3
+function solution(code) {
+  let odd = false;
+  return (
+    Array.from(code).reduce((acc, v, i) => {
+      if (v === "1") {
+        odd = !odd;
+        return acc;
+      }
+      return i % 2 === (odd ? 1 : 0) ? acc + v : acc;
+    }, "") || "EMPTY"
+  );
+}
