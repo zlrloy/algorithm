c3b71e8f4c466fc45b7dd0f27d3f88a0ebc5e5ae
yjieuny <asd654@hanmail.net>
2024-07-09 23:13:08 +0900

    add: 공백으로 구분하기 1 JavaScript 제출

---
+++ Programers/Lv. 0/공백으로 구분하기 1.js
@@ -0,0 +1,14 @@
+/*
+문제. 공백으로 구분하기 1
+단어가 공백 한 개로 구분되어 있는 문자열 my_string이 매개변수로 주어질 때,
+my_string에 나온 단어를 앞에서부터 순서대로 담은 문자열 배열을 return 하는 solution 함수를 작성해 주세요.
+*/
+
+//Case1
+function solution(my_string) {
+  let result = my_string.split(/\s+/g);
+  return result;
+}
+
+console.log(solution("i love you")); //["i", "love", "you"]
+console.log(solution("programmers")); //["programmers"]
