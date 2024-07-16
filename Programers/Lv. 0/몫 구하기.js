cad51ad4dcfe10165ea31d3b891e8d56df58a9b5
yjieuny <asd654@hanmail.net>
2024-07-16 09:26:54 +0900

    add: 몫 구하기 JavaScript 제출

---
+++ Programers/Lv. 0/몫 구하기.js
@@ -0,0 +1,12 @@
+/*
+문제. 몫 구하기
+정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+*/
+
+function solution(num1, num2) {
+  const divide = num1 / num2;
+  return Math.floor(divide);
+}
+
+console.log(solution(10, 5)); //2
+console.log(solution(7, 2)); //3
