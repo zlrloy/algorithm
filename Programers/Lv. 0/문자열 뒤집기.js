219ee9e1b4365dc8cb55d7541e371fd78a6dd5df
yjieuny <asd654@hanmail.net>
2024-08-08 10:27:29 +0900

    update: 문자열 뒤집기 문제2 추가

--- Programers/Lv. 0/문자열 뒤집기.js
+++ Programers/Lv. 0/문자열 뒤집기.js
@@ -1,5 +1,5 @@
 /*
-문제. 문자열 뒤집기
+문제1. 문자열 뒤집기
 문자열 my_string과 정수 s, e가 매개변수로 주어질 때, my_string에서 인덱스 s부터 인덱스 e까지를 뒤집은 문자열을 return 하는 solution 함수를 작성해 주세요.
 */
 
@@ -27,3 +27,15 @@ function solution(my_string, s, e) {
 
   return my_string.replace(str, newStr); //str 자리에 newStr을 넣어 return
 }
+
+/*
+문제2. 문자열 뒤집기
+문자열 my_string이 매개변수로 주어집니다. my_string을 거꾸로 뒤집은 문자열을 return하도록 solution 함수를 완성해주세요.
+*/
+
+function solution(my_string) {
+  return [...my_string].reverse().join("");
+}
+
+console.log(solution("jaron")); //"noraj"
+console.log(solution("bread")); //"daerb"
