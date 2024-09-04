ffce5ef602c836adf3c36951132a68eadda8f07f
yjieuny <asd654@hanmail.net>
2024-09-04 19:13:44 +0900

    add: 숨어있는 숫자의 덧셈(1) JavaScript 제출

---
+++ Programers/Lv. 0/숨어있는 숫자의 덧셈 (1).js
@@ -0,0 +1,13 @@
+/*
+문제. 숨어있는 숫자의 덧셈 (1)
+문자열 my_string이 매개변수로 주어집니다. my_string안의 모든 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
+*/
+
+function solution(my_string) {
+  const numbers = [...my_string].filter((i) => !isNaN(i)).map(Number);
+
+  return numbers.reduce((acc, curr) => acc + curr, 0);
+}
+
+console.log(solution("aAb1B2cC34oOp")); //10
+console.log(solution("1a2b3c4d123")); //16
