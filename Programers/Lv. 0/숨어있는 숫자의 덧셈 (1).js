3914d14af384e5de92d31ec9390878e373d1ca9a
yjieuny <asd654@hanmail.net>
2024-09-05 17:35:04 +0900

    update: 숨어있는 숫자의 덧셈(1) Case2 추가

--- Programers/Lv. 0/숨어있는 숫자의 덧셈 (1).js
+++ Programers/Lv. 0/숨어있는 숫자의 덧셈 (1).js
@@ -3,6 +3,7 @@
 문자열 my_string이 매개변수로 주어집니다. my_string안의 모든 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
 */
 
+//Case1
 function solution(my_string) {
   const numbers = [...my_string].filter((i) => !isNaN(i)).map(Number);
 
@@ -11,3 +12,8 @@ function solution(my_string) {
 
 console.log(solution("aAb1B2cC34oOp")); //10
 console.log(solution("1a2b3c4d123")); //16
+
+//Case2
+function solution(my_string) {
+  return [...my_string].reduce((acc, cur) => (Number(cur) ? +acc + +cur : acc), 0);
+}
