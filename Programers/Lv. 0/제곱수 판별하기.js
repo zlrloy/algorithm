8f0bdb8e0648b6e3128962ffead2da2383b967c8
yjieuny <asd654@hanmail.net>
2024-09-15 21:35:28 +0900

    add: 제곱수 판별하기 JavaScript 제출

---
+++ Programers/Lv. 0/제곱수 판별하기.js
@@ -0,0 +1,11 @@
+/*
+문제. 제곱수 판별하기
+어떤 자연수를 제곱했을 때 나오는 정수를 제곱수라고 합니다. 정수 n이 매개변수로 주어질 때, n이 제곱수라면 1을 아니라면 2를 return하도록 solution 함수를 완성해주세요.
+*/
+
+function solution(n) {
+  return Math.sqrt(n) % 1 === 0 ? 1 : 2;
+}
+
+console.log(solution(144)); //1
+console.log(solution(976)); //2
