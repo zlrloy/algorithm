e162645b8ba26446def65cbbd753332c7b009182
yjieuny <asd654@hanmail.net>
2024-09-16 21:49:34 +0900

    add: 세균 증식 JavaScript 제출

---
+++ Programers/Lv. 0/세균 증식.js
@@ -0,0 +1,15 @@
+/*
+문제. 세균 증식
+어떤 세균은 1시간에 두배만큼 증식한다고 합니다. 처음 세균의 마리수 n과 경과한 시간 t가 매개변수로 주어질 때 t시간 후 세균의 수를 return하도록 solution 함수를 완성해주세요.
+*/
+
+//Case1
+function solution(n, t) {
+  for (let i = 1; i <= t; i++) {
+    n *= 2;
+  }
+  return n;
+}
+
+console.log(solution(2, 10)); //2048
+console.log(solution(7, 15)); //229,376
