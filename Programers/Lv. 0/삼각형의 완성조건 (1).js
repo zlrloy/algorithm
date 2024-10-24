4766bf3272174371e748f282261ee38cceae7ba0
yjieuny <asd654@hanmail.net>
2024-10-24 21:52:00 +0900

    update: 삼각형의 완성조건(1) Case2 추가

--- Programers/Lv. 0/삼각형의 완성조건 (1).js
+++ Programers/Lv. 0/삼각형의 완성조건 (1).js
@@ -5,6 +5,7 @@
 삼각형의 세 변의 길이가 담긴 배열 sides이 매개변수로 주어집니다. 세 변으로 삼각형을 만들 수 있다면 1, 만들 수 없다면 2를 return하도록 solution 함수를 완성해주세요.
 */
 
+//Case1
 function solution(sides) {
   const hypotenuse = Math.max(...sides);
   const hypotenuseIndex = sides.indexOf(hypotenuse);
@@ -16,3 +17,9 @@ function solution(sides) {
 console.log(solution([1, 2, 3])); //2
 console.log(solution([3, 6, 2])); //2
 console.log(solution([199, 72, 222])); //1
+
+//Case2
+function solution(sides) {
+  const sides = sides.sort((a, b) => a - b);
+  return sides[0] + sides[1] > sides[2] ? 1 : 2;
+}
