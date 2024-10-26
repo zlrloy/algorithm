d732a5fc99b16e21ffddb16c7e77f82ab56811b5
yjieuny <asd654@hanmail.net>
2024-10-26 22:04:45 +0900

    update: 문자 반복 출력하기 Case2 추가

--- Programers/Lv. 0/문자 반복 출력하기.js
+++ Programers/Lv. 0/문자 반복 출력하기.js
@@ -3,9 +3,15 @@
 문자열 my_string과 정수 n이 매개변수로 주어질 때, my_string에 들어있는 각 문자를 n만큼 반복한 문자열을 return 하도록 solution 함수를 완성해보세요.
 */
 
+//Case1
 function solution(my_string, n) {
   const string = [...my_string];
   return string.flatMap((char) => Array(n).fill(char)).join("");
 }
 
 console.log(solution("hello", 3)); //"hhheeellllllooo"
+
+//Case2
+function solution(my_string, n) {
+  return [...my_string].map((v) => v.repeat(n)).join("");
+}
