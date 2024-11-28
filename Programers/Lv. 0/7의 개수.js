dbc84740508499a06738bbabd7410498080a49b3
yjieuny <asd654@hanmail.net>
2024-11-28 09:52:19 +0900

    update: 7의 개수 JavaScript 제출

--- Programers/Lv. 0/7의 개수.js
+++ Programers/Lv. 0/7의 개수.js
@@ -1,5 +1,6 @@
 /*
-문제. 머쓱이는 행운의 숫자 7을 가장 좋아합니다. 정수 배열 array가 매개변수로 주어질 때, 7이 총 몇 개 있는지 return 하도록 solution 함수를 완성해보세요.
+풀이. 7의 개수
+ 머쓱이는 행운의 숫자 7을 가장 좋아합니다. 정수 배열 array가 매개변수로 주어질 때, 7이 총 몇 개 있는지 return 하도록 solution 함수를 완성해보세요.
 */
 
 //Case1
@@ -8,4 +9,9 @@ function solution(array) {
   const countOfSevens = (joinedString.match(/7/g) || []).length;
 
   return countOfSevens;
+}
+
+//Case2
+function solution(array) {
+  return array.join("").split("7").length - 1;
 }
