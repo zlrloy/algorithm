449c4fc4d5a6b751b460c8e80c019c02a6de071b
yjieuny <asd654@hanmail.net>
2024-12-02 13:34:56 +0900

    add: 한번만 등장한 문자 JavaScript 제출

---
+++ Programers/Lv. 0/한번만 등장한 문자.js
@@ -0,0 +1,17 @@
+/*
+문제. 한번만 등장한 문자
+문자열 s가 매개변수로 주어집니다. s에서 한 번만 등장하는 문자를 사전 순으로 정렬한 문자열을 return 하도록 solution 함수를 완성해보세요.
+한 번만 등장하는 문자가 없을 경우 빈 문자열을 return 합니다.
+*/
+
+function solution(inputString) {
+  let result = [];
+
+  for (let char of inputString) if (inputString.indexOf(char) === inputString.lastIndexOf(char)) result.push(char);
+
+  return result.sort().join("");
+}
+
+console.log(solution("abcabcadc")); //"d"
+console.log(solution("abdc")); //"abcd"
+console.log(solution("hello")); //"eho"
