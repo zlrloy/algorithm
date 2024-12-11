3625dc78e0d71c16b8113e41bfb01a707ccce6d3
yjieuny <asd654@hanmail.net>
2024-12-11 16:47:48 +0900

    add: 숨어있는 숫자의 덧셈2 JavaScript 제출

---
+++ Programers/Lv. 0/숨어있는 숫자의 덧셈 2.js
@@ -0,0 +1,11 @@
+/*
+문제. 숨어있는 숫자의 덧셈 (2)
+문자열 my_string이 매개변수로 주어집니다. my_string은 소문자, 대문자, 자연수로만 구성되어있습니다. my_string안의 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
+*/
+
+function solution(my_string) {
+  return my_string.split(/[a-zA-Z]/g).reduce((acc, curr) => +acc + +curr, 0);
+}
+
+console.log(solution("aAb1B2cC34oOp")); //37
+console.log(solution("1a2b3c4d123Z")); //133
