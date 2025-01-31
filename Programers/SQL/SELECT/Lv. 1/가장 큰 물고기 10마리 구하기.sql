b4b086696e819b29b051ab65e0972eef633929b1
yjieuny <asd654@hanmail.net>
2025-01-31 14:57:56 +0900

    add: 가장 큰 물고기 10마리 구하기 MySQL 제출

---
+++ Programers/SQL/SELECT/Lv. 1/가장 큰 물고기 10마리 구하기.sql
@@ -0,0 +1,23 @@
+/*
+문제 설명
+낚시앱에서 사용하는 FISH_INFO 테이블은 잡은 물고기들의 정보를 담고 있습니다.
+FISH_INFO 테이블의 구조는 다음과 같으며 ID, FISH_TYPE, LENGTH, TIME은 각각 잡은 물고기의 ID, 물고기의 종류(숫자), 잡은 물고기의 길이(cm), 물고기를 잡은 날짜를 나타냅니다.
+
+Column name	Type	Nullable
+ID	INTEGER	FALSE
+FISH_TYPE	INTEGER	FALSE
+LENGTH	FLOAT	TRUE
+TIME	DATE	FALSE
+단, 잡은 물고기의 길이가 10cm 이하일 경우에는 LENGTH 가 NULL 이며, LENGTH 에 NULL 만 있는 경우는 없습니다.
+
+문제
+FISH_INFO 테이블에서 가장 큰 물고기 10마리의 ID와 길이를 출력하는 SQL 문을 작성해주세요.
+결과는 길이를 기준으로 내림차순 정렬하고, 길이가 같다면 물고기의 ID에 대해 오름차순 정렬해주세요. 단, 가장 큰 물고기 10마리 중 길이가 10cm 이하인 경우는 없습니다.
+ID 컬럼명은 ID, 길이 컬럼명은 LENGTH로 해주세요.
+*/
+
+-- 풀이
+SELECT ID, LENGTH
+FROM FISH_INFO
+ORDER BY LENGTH DESC, ID ASC
+LIMIT 10;
