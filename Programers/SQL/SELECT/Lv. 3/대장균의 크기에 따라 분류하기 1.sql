412f1f261ca645000210939b156317c6a9ca0c41
yjieuny <asd654@hanmail.net>
2025-02-09 16:33:46 +0900

    add: 대장균의 크기에 따라 분류하기 1 MySQL 제출

---
+++ Programers/SQL/SELECT/Lv. 3/대장균의 크기에 따라 분류하기 1.sql
@@ -0,0 +1,28 @@
+/*
+문제 설명
+대장균들은 일정 주기로 분화하며, 분화를 시작한 개체를 부모 개체, 분화가 되어 나온 개체를 자식 개체라고 합니다.
+다음은 실험실에서 배양한 대장균들의 정보를 담은 ECOLI_DATA 테이블입니다.
+ECOLI_DATA 테이블의 구조는 다음과 같으며, ID, PARENT_ID, SIZE_OF_COLONY, DIFFERENTIATION_DATE, GENOTYPE 은 각각 대장균 개체의 ID, 부모 개체의 ID, 개체의 크기, 분화되어 나온 날짜, 개체의 형질을 나타냅니다.
+
+Column name	Type	Nullable
+ID	INTEGER	FALSE
+PARENT_ID	INTEGER	TRUE
+SIZE_OF_COLONY	INTEGER	FALSE
+DIFFERENTIATION_DATE	DATE	FALSE
+GENOTYPE	INTEGER	FALSE
+최초의 대장균 개체의 PARENT_ID 는 NULL 값입니다.
+
+문제
+대장균 개체의 크기가 100 이하라면 'LOW', 100 초과 1000 이하라면 'MEDIUM', 1000 초과라면 'HIGH' 라고 분류합니다.
+대장균 개체의 ID(ID) 와 분류(SIZE)를 출력하는 SQL 문을 작성해주세요.이때 결과는 개체의 ID 에 대해 오름차순 정렬해주세요.
+*/
+
+-- 풀이
+SELECT ID, 
+    CASE 
+        WHEN SIZE_OF_COLONY <= 100 THEN 'LOW'
+        WHEN SIZE_OF_COLONY <= 1000 THEN 'MEDIUM'
+        ELSE 'HIGH'
+    END AS SIZE
+FROM ECOLI_DATA
+ORDER BY ID;
