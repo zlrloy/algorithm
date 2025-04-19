3ddaed784ca30b1ad941487bd90e879acaa3a543
yjieuny <asd654@hanmail.net>
2025-04-20 00:31:12 +0900

    add: 조건에 맞는 사원 정보 조회하기 MySQL 제출

---
+++ Programers/SQL/Lv. 2/조건에 맞는 사원 정보 조회하기.sql
@@ -0,0 +1,42 @@
+/*
+문제 설명
+HR_DEPARTMENT 테이블은 회사의 부서 정보를 담은 테이블입니다. HR_DEPARTMENT 테이블의 구조는 다음과 같으며 DEPT_ID, DEPT_NAME_KR, DEPT_NAME_EN, LOCATION은 각각 부서 ID, 국문 부서명, 영문 부서명, 부서 위치를 의미합니다.
+
+Column name	Type	Nullable
+DEPT_ID	VARCHAR	FALSE
+DEPT_NAME_KR	VARCHAR	FALSE
+DEPT_NAME_EN	VARCHAR	FALSE
+LOCATION	VARCHAR	FLASE
+HR_EMPLOYEES 테이블은 회사의 사원 정보를 담은 테이블입니다. HR_EMPLOYEES 테이블의 구조는 다음과 같으며 EMP_NO, EMP_NAME, DEPT_ID, POSITION, EMAIL, COMP_TEL, HIRE_DATE, SAL은 각각 사번, 성명, 부서 ID, 직책, 이메일, 전화번호, 입사일, 연봉을 의미합니다.
+
+Column name	Type	Nullable
+EMP_NO	VARCHAR	FALSE
+EMP_NAME	VARCHAR	FALSE
+DEPT_ID	VARCHAR	FALSE
+POSITION	VARCHAR	FALSE
+EMAIL	VARCHAR	FALSE
+COMP_TEL	VARCHAR	FALSE
+HIRE_DATE	DATE	FALSE
+SAL	NUMBER	FALSE
+HR_GRADE 테이블은 2022년 사원의 평가 정보를 담은 테이블입니다. HR_GRADE의 구조는 다음과 같으며 EMP_NO, YEAR, HALF_YEAR, SCORE는 각각 사번, 연도, 반기, 평가 점수를 의미합니다.
+
+Column name	Type	Nullable
+EMP_NO	VARCHAR	FALSE
+YEAR	NUMBER	FALSE
+HALF_YEAR	NUMBER	FALSE
+SCORE	NUMBER	FALSE
+
+문제
+HR_DEPARTMENT, HR_EMPLOYEES, HR_GRADE 테이블에서 2022년도 한해 평가 점수가 가장 높은 사원 정보를 조회하려 합니다.
+2022년도 평가 점수가 가장 높은 사원들의 점수, 사번, 성명, 직책, 이메일을 조회하는 SQL문을 작성해주세요.
+2022년도의 평가 점수는 상,하반기 점수의 합을 의미하고, 평가 점수를 나타내는 컬럼의 이름은 SCORE로 해주세요.
+*/
+
+-- 풀이
+SELECT SUM(HG.SCORE) AS SCORE, HE.EMP_NO, HE.EMP_NAME, HE.POSITION, HE.EMAIL
+FROM HR_EMPLOYEES HE
+INNER JOIN HR_GRADE HG ON HG.EMP_NO = HE.EMP_NO
+GROUP BY YEAR, HE.EMP_NO
+HAVING HG.YEAR = '2022'
+ORDER BY SCORE DESC
+LIMIT 1;
